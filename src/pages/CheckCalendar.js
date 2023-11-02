5c9299a5cc819e56d0e6aa077dc32569f0bd8020
Prathmesh Bhatt <ppb190002@utdallas.edu>
2023-11-02 17:05:52 -0500

    CalendarComponentAdded

---
+++ src/pages/CheckCalendar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { Typography } from '@mui/material'
+import FilterButtonGroup from '../FilterButtonGroup/FilterButtonGroup'
+import Calendar from '../components/Calendar/Calendar'
+const CheckCalendar = () => {
+  return (
+    <div name="CheckCalendar">
+<Navbar/>
+<Typography
+  style={{
+    fontSize: '40px',
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    textAlign: 'left', // Shift to the left
+    color: "grey",
+    marginLeft: "55px", 
+    marginTop: "30px"
+  }}
+>
+  Hello,
+</Typography>
+<div style={{display: "flex"}}>
+<Typography
+  style={{
+    fontSize: '40px',
+    fontFamily: 'Roboto',
+    fontWeight: 600,
+    textAlign: 'left', // Shift to the left
+    color: "black",
+    marginLeft: "55px", 
+    marginTop: "10px"
+  }}
+>
+ Thomas!
+</Typography>
+<FilterButtonGroup></FilterButtonGroup>
+
+</div>
+<Calendar></Calendar>
+    </div>
+  )
+}
+
+export default CheckCalendar
